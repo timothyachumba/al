2f3491155ac075bd67701601e89e9a589a2b5990
Timothy Achumba <timothyachumba@gmail.com>
2024-04-03 20:40:03 +0100

    feat: Add new hero block for Balance Hero page
    Added a new hero block for the Balance Hero page with fields for tagline,
    timing, and image. Included PHP template to display tagline, image, and
    subscription form. Added interactive SVG animation when hovering over the
    image.

--- src/js/project.js
+++ src/js/project.js
@@ -133,7 +133,27 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     document.getElementById('artisan').style.transform = `scale(${1 + (artisanProgress / 2)})`
   })
+  
+  const box = document.querySelector('.project-hero__image__container img');
+  const logo = document.querySelector('.project-hero__image__container svg');
 
+  document.addEventListener('mousemove', function(e) {
+    const mouseX = e.clientX; // Mouse X position
+    const mouseY = e.clientY; // Mouse Y position
+    const windowWidth = window.innerWidth / 2; // Half of window width
+    const windowHeight = window.innerHeight / 2; // Half of window height
+
+    // Calculate difference between mouse position and center of the screen
+    const diffX = (windowWidth - mouseX) / 40;
+    const diffY = (windowHeight - mouseY) / 40;
+
+    const diffX2 = (windowWidth - mouseX) / 80;
+    const diffY2 = (windowHeight - mouseY) / 80;
+
+    // Apply the transformation
+    box.style.transform = `translate(${diffX}px, ${diffY}px)`;
+    logo.style.transform = `translate(${diffX2}px, ${diffY2}px)`;
+  });
 
   Marquee('#marquee', 0.5)
 
