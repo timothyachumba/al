c15f3db64d90fb1b47f2c8bd73dbb38e986611bf
Timothy Achumba <timothyachumba@gmail.com>
2025-01-13 15:04:41 +0000

    started working on route for pages

--- site/templates/newsletters.php
+++ site/templates/newsletters.php
@@ -1,1 +1,17 @@
-<?php go($page->children()->listed()->first()?->url() ?? 'home');
+<?php
+
+$action = get('action');
+
+if ($action === 'confirm') {
+ echo "this is a confirmation page";
+} elseif ($action === 'subscribe')  {
+  // Handle subscribe
+} elseif ($action === 'alreadysubscribed')  {
+  // Handle already subscribed
+} elseif ($action === 'unsubscribed') {
+  // Handle unsubscribed
+} else {
+  go($page->children()->listed()->first()?->url() ?? 'home');
+  exit;
+}
+?>
